b403d0c0b4f66c78eeaa3dcb8a7abc9aaeb961bf
Matheus Emanoel <buss.matheusemanoel@gmail.com>
2025-02-23 17:29:29 -0300

    feat: configure tailwind and design system

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div>
-      <h1>Hello world</h1>
+    <div className="flex-center h-screen">
+      <h1 className="text-8xl text-brand">Test</h1>
     </div>
   );
 }
