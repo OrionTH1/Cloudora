6e566c88b51ff457056e8b9eee06cf8addc7d482
Matheus Emanoel <buss.matheusemanoel@gmail.com>
2025-03-12 18:07:40 -0300

    feat: create getFiles func to get user files

--- src/lib/actions/file.actions.ts
+++ src/lib/actions/file.actions.ts
@@ -7,8 +7,9 @@ import {
   DATABASE_ID,
   FILES_COLLECTION_ID,
 } from "../appwrite/config";
-import { ID } from "node-appwrite";
+import { ID, Models, Query } from "node-appwrite";
 import { revalidatePath } from "next/cache";
+import { getCurrentUser } from "./user.actions";
 
 export const uploadFiles = async (
   file: File,
@@ -55,5 +56,36 @@ export const uploadFiles = async (
     return newFile;
   } catch (error) {
     handleError(error, "failed to upload files");
+  }
+};
+
+export const createQueries = async (currentUser: Models.Document) => {
+  const queries = [
+    Query.or([
+      Query.equal("owner", [currentUser.$id]),
+      Query.contains("users", [currentUser.email]),
+    ]),
+  ];
+
+  return queries;
+};
+
+export const getFiles = async () => {
+  const { database } = await createAdminClient();
+
+  try {
+    const currentUser = await getCurrentUser();
+    if (!currentUser) throw new Error("User not found");
+
+    const queries = await createQueries(currentUser);
+    const files = await database.listDocuments(
+      DATABASE_ID!,
+      FILES_COLLECTION_ID!,
+      queries
+    );
+
+    return files;
+  } catch (error) {
+    handleError(error, "Failed to get files");
   }
 };
