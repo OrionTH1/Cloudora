8e2b3f8753361ebeb4a5ca3d1758a702d6bb6e07
Matheus Emanoel <buss.matheusemanoel@gmail.com>
2025-03-13 19:32:24 -0300

    feat: implement Sort files component

--- src/components/Sort.tsx
+++ src/components/Sort.tsx
@@ -1,5 +1,34 @@
+"use client";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { sortTypes } from "@/constants";
+import { usePathname, useRouter } from "next/navigation";
+
 function Sort() {
-  return <div>Sort</div>;
+  const path = usePathname();
+  const router = useRouter();
+  const handleSort = (value: string) => {
+    router.push(`${path}?sort=${value}`);
+  };
+  return (
+    <Select onValueChange={handleSort} defaultValue={sortTypes[0].value}>
+      <SelectTrigger className="sort-select">
+        <SelectValue placeholder={sortTypes[0].value} />
+      </SelectTrigger>
+      <SelectContent className="sort-select-content">
+        {sortTypes.map((type) => (
+          <SelectItem key={type.value} value={type.value}>
+            {type.label}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  );
 }
 
 export default Sort;
