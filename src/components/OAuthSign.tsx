d51f002b9fbdeee3a0f6a788d76d9bf90087ec25
Matheus Emanoel <buss.matheusemanoel@gmail.com>
2025-04-05 18:13:59 -0300

    feat: implement google oauth

--- src/components/OAuthSign.tsx
+++ src/components/OAuthSign.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { verifySecret } from "@/lib/actions/user.actions";
+import { createSessionSecret } from "@/lib/actions/user.actions";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
@@ -8,8 +8,8 @@ function OAuthSign({ secret, userId }: { userId: string; secret: string }) {
   const router = useRouter();
   useEffect(() => {
     const handleOAuth = async () => {
-      await verifySecret(userId as string, secret as string);
-      router.push("/");
+      await createSessionSecret(userId as string, secret as string, "");
+      router.push("/cloud");
     };
 
     handleOAuth();
