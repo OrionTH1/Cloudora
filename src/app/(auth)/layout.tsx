bc783528448172580a2cb5416fdadba4bb294cdb
Matheus Emanoel <buss.matheusemanoel@gmail.com>
2025-04-05 18:07:34 -0300

    styles: edit root layout styles

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,7 +1,7 @@
 function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex min-h-screen">
-      <section className="flex flex-1 flex-col items-center bg-white p-4 py-10 lg:justify-center lg:p-10 lg:py-0">
+      <section className="flex flex-1 flex-col items-center justify-center bg-white p-4 py-10">
         {children}
       </section>
     </div>
